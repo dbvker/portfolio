10102994bd9eb3ec0cccac300abda3621064fbdc
Dylan Baker <dylanmbaker@yahoo.com>
2022-06-17 11:57:45 -0500

    fixed header on mobile nav

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -51,7 +51,7 @@ function Header() {
                 <div className='menu-nav-container'>
                     <div className='menu-nav-header'>
                         <ul className='header-link'>
-                            <p><strong>Dylan Baker</strong></p>
+                            <p>Dylan Baker</p>
                             <li>
                                 <a href='https://www.linkedin.com/in/dylanmbaker/' target='_blank' rel='noreferrer'>
                                     <img src={LinkedInIcon} alt='LinkedIn logo' />
